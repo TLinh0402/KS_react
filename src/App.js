5a232c57e73d0900491e7adf4f6bc953a2db47e1
TLinh0402 <linhntt.22itb@vku.udn.vn>
2025-06-16 09:36:55 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SearchBar from './SearchBar';
 
 function App() {
+  const handleSearch = (query) => {
+    console.log('Từ khóa tìm kiếm:', query);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginTop: 30 }}>
+  
+      <SearchBar onSearch={handleSearch} />
+      {/* Các thành phần khác */}
     </div>
   );
 }
---
+++ src/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+    const [query, setQuery] = useState('');
+
+    return (
+        <div style={{ margin: 10 }}>
+            <div style={{ marginBottom: 5, fontWeight: 'bold' }}>
+                Nhập thông tin
+            </div>
+            <div style={{ display: 'flex', margin: 10 }}>
+                <input
+                    style={{ flex: 1, padding: 8, marginRight: 10, borderRadius: 5, border: '1px solid #ccc' }}
+                    type="text"
+                    placeholder="Nhập từ khóa tìm kiếm..."
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
+                />
+                <button onClick={() => onSearch(query)}>Tìm kiếm</button>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default SearchBar;
